36c40bb348e67727d1e0569edcfb7e6deb582ee0
tcyeee <tcyeee@outlook.com>
2025-05-23 21:36:29 +0800

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = '狐狸的胸针';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const BASE_INFO = {
+    author: "tcyeee",
+}
+export const LINK = {
+    github: "https://github.com/tcyeee",
+    bilibili: 'https://space.bilibili.com/10879225',
+    redNote: 'https://www.xiaohongshu.com/user/profile/5b9cf40e9a9c9300014062b4',
+}
