67df2e0b2f10b829c3010bbfbf7091a9b12d5c79
tcyeee <tcyeee@outlook.com>
2025-05-26 14:46:45 +0800

    project_1

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
         '@components': path.resolve('./src/components'),
         '@layouts': path.resolve('./src/layouts'),
         '@styles': path.resolve('./src/styles'),
+        '@assets': path.resolve('./src/assets'),
         '@config': path.resolve('./src/config.ts')
       }
     }
