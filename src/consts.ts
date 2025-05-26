50f4fb46b738dc90e4ed40a0ee65aef3c1dc6b8e
tcyeee <tcyeee@outlook.com>
2025-05-26 10:42:14 +0800

    add tags

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,3 @@ export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const BASE_INFO = {
     author: "tcyeee",
 }
-export const LINK = {
-    twitter: "https://x.com/tcyeee",
-    github: "https://github.com/tcyeee",
-    bilibili: 'https://space.bilibili.com/10879225',
-    redNote: 'https://www.xiaohongshu.com/user/profile/5b9cf40e9a9c9300014062b4',
-}
