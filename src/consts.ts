33837a5d6e26c549257e2c67c1500fd3f91a0bc7
tcyeee <tcyeee@outlook.com>
2025-05-26 10:10:50 +0800

    index init

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,10 @@
-export const SITE_TITLE = '狐狸的胸针';
+export const SITE_TITLE = 'Hello, I\'m Lucas.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const BASE_INFO = {
     author: "tcyeee",
 }
 export const LINK = {
+    twitter: "https://x.com/tcyeee",
     github: "https://github.com/tcyeee",
     bilibili: 'https://space.bilibili.com/10879225',
     redNote: 'https://www.xiaohongshu.com/user/profile/5b9cf40e9a9c9300014062b4',
