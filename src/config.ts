6ce006e1ee82fceb7bc5ff1c237463ec36951ef2
tcyeee <tcyeee@outlook.com>
2025-05-27 16:56:37 +0800

    project added

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,5 @@ export const BASE_INFO = {
     projectTitle: "为政府提供的技术支持",
     projectDescription: "我目前在为浙江日报及丽水市政府提供技术支持, 涉及到政策分发, 游戏, 人工智能等.",
     interestTitle: "因为兴趣而开发的一些项目",
-    interestDescription: "我对人工智能, 区块链, 游戏等领域有浓厚的兴趣, 在业余时间做出了不少有趣的东西, 或许有你喜欢的😆. 我偶尔也会写一些新的东西, 所以, 可以时常回来看看😁.",
+    interestDescription: "我在业余时间做出了不少有趣的东西, 或许有你喜欢的😆. \r\n项目代码基本都会开源, 源码于GITHUB, 我偶尔也会写一些新的东西, 所以, 可以时常回来看看😁.",
 }
