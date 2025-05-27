c74388ceefc1438fdb877fc49b3bed0b6f7ba995
tcyeee <tcyeee@outlook.com>
2025-05-27 13:02:29 +0800

    更新中文支持

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,8 @@ export const SITE_TITLE = 'Hello, I\'m Lucas.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const BASE_INFO = {
     author: "tcyeee",
+    title: "Hello, I'm Lucas.",
+    description: "I'm a full-stack backend developer with 7 years of experience, mainly in enterprise software. I've also worked on games and blockchain projects, gaining broad technical exposure and adaptability.",
+    projectTitle: "独立开发的东西",
+    projectDescription: "这里是我最近写的项目, 我偶尔会写一些新的东西, 所以, 可以时常回来看看😁. 如果你想知道我在工作中写过的项目, 可以去看看我的简历.",
 }
