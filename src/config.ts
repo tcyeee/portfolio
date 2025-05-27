1684a4b3a68049eba097079cad316aea33ab1ebe
tcyeee <tcyeee@outlook.com>
2025-05-27 14:24:34 +0800

    project add

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const SITE_TITLE = '你好👋,欢迎来到我的博客';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const BASE_INFO = {
     author: "tcyeee",
-    title: "Hello, I'm Lucas.",
+    title: "你好👋",
     description: "我是一名后端全栈开发, 有7年工作经验, 主要做企业、银行、市政软件开发, 当然工作之余我也会做一些其他项目, 比如人工智能, 区块链, 游戏MOD.",
     projectTitle: "独立开发的东西",
-    projectDescription: "这里是我最近写的项目, 我偶尔会写一些新的东西, 所以, 可以时常回来看看😁. 如果你想知道我在工作中写过的项目, 可以去看看我的简历.",
+    projectDescription: "这里是我最近一个人开发的项目, 我偶尔也会写一些新的东西, 所以, 可以时常回来看看😁.",
 }
