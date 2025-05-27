ba6d31ad05488ab011b2df5ad853135d375f9fd7
tcyeee <tcyeee@outlook.com>
2025-05-27 14:42:36 +0800

    颜色调整

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,8 @@ export const BASE_INFO = {
     author: "tcyeee",
     title: "你好👋",
     description: "我是一名后端全栈开发, 有7年工作经验, 主要做企业、银行、市政软件开发, 当然工作之余我也会做一些其他项目, 比如人工智能, 区块链, 游戏MOD.",
-    projectTitle: "独立开发的东西",
-    projectDescription: "这里是我最近一个人开发的项目, 我偶尔也会写一些新的东西, 所以, 可以时常回来看看😁.",
+    projectTitle: "为政府提供的技术支持",
+    projectDescription: "我目前在为浙江日报及丽水市政府提供技术支持, 涉及到政策分发, 游戏, 人工智能等.",
+    interestTitle: "因为兴趣而开发的一些项目",
+    interestDescription: "我对人工智能, 区块链, 游戏等领域有浓厚的兴趣, 在业余时间做出了不少有趣的东西, 或许有你喜欢的😆. 我偶尔也会写一些新的东西, 所以, 可以时常回来看看😁.",
 }
