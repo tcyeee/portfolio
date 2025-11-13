ee1b7185ad5f1c157b59662f691a3f5ccb2cc015
tcyeee <tcyeee@outlook.com>
2025-11-14 00:33:06 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const BASE_INFO = {
     description: "我是一名后端全栈开发, 有7年工作经验, 主要做企业、银行、市政软件开发, 当然工作之余我也会做一些其他项目, 比如人工智能, 区块链, 游戏MOD.",
     workTitle: "工作经历",
     workDescription: "我有7年工作经验, 主要从事企业、银行、市政软件开发, 目前在浙江日报集团工作, 主要负责后端开发, 也会做一些前端工作.",
-    projectTitle: "为政府提供的技术支持",
+    projectTitle: "技术支持案例",
     projectDescription: "目前我在为浙江日报及丽水市政府提供技术支持, 涉及到政策分发, 游戏, 人工智能等.",
     interestTitle: "因为兴趣而开发的一些项目",
     interestDescription: "我在业余时间做出了不少有趣的东西, 或许有你喜欢的😆. \r\n项目代码基本都会开源, 源码于GITHUB, 我偶尔也会写一些新的东西, 所以, 可以时常回来看看😁.",
